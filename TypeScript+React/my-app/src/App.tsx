11c279e3dfdc20454981a2ced62e95376d7a817a
Doit <duhee7524@naver.com>
2024-01-18 11:13:04 +0900

    state와 props 로직 구현
    타입스크립트에서 부모 자식간의 state와 props 주고받는 로직 구현

---
+++ TypeScript+React/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos(prevState => {
+      return prevState.concat(newTodo);
+    })
+  }
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ TypeScript+React/my-app/src/components/Todos/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Todo from '../../models/todo';
+import TodoItem from '../TodoItem';
+
+type TodosProps ={
+    items: Todo[]
+}
+
+const Todos:React.FC<TodosProps> = (props) => {
+    return (
+        <ul>
+            {props.items.map(item => <TodoItem text ={item.text}/>)}
+        </ul>
+    );
+};
+
+export default Todos;
